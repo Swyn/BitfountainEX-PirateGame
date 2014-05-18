191865f5ebc5b1639cdd4c564cf228dd62526e0b
Alexandre ARRIGHI <alexandrearrighi@MacBook-Pro-de-Alexandre.local>
2014-05-18 03:17:36 +0200

    Initial Commit

---
+++ Pirate Game/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pirate Game
+//
+//  Created by Alexandre ARRIGHI on 18/05/2014.
+//  Copyright (c) 2014 Alexandre ARRIGHI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AAAppDelegate class]));
+    }
+}
